508538df3575057621de932d4ce758a3cb3ef342
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2024-10-25 15:42:42 +0200

    Solve level 3

--- ccc/contest.py
+++ ccc/contest.py
@@ -5,20 +5,26 @@ def solve(data: dict) -> str:
     rooms = data["rooms"]
 
     output = []
-
-    for x, y, _ in rooms:
+    for x, y, ndesks in rooms:
         r = Room(x, y)
-        r.fill_room_with_tables()
-        output.append(r.print_room())
+        nplaced = r.fill_room()
+        assert (
+            nplaced == ndesks
+        ), f"should have placed {ndesks} desks, but placed {nplaced}"
+        output.append(r.room_matrix_str())
 
     return "\n\n".join(map(str, output))
+
+
+EMPTY = 0
 
 
 class Room:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-        self.matrix = self._init_matrix()
+        self.matrix = {}  # self._init_matrix()
+        self.ntables = 0  # number of placed tables
 
     def _init_matrix(self):
         matrix = {}
@@ -27,21 +33,65 @@ class Room:
                 matrix[complex(ix, iy)] = 0
         return matrix
 
-    def fill_room_with_tables(self):
-        table = 1
+    def fill_room(self):
+        nr = 1
         for iy in range(self.y):
-            for ix in range(0, self.x, 3):
-                self.matrix[complex(ix, iy)] = table
-                self.matrix[complex(ix + 1, iy)] = table
-                self.matrix[complex(ix + 2, iy)] = table
-                table += 1
-        return self.matrix
+            for ix in range(self.x):
+                if self.place_table(ix, iy, nr):
+                    nr += 1
+                if self.place_table(ix, iy, nr, vertical=True):
+                    nr += 1
+        return nr - 1  # number of placed tables
 
-    def print_room(self) -> str:
-        lines = []
+    # def fill_room(self):
+    #     table = 1
+    #     for iy in range(self.y):
+    #         for ix in range(0, self.x, 3):
+    #             self.matrix[complex(ix, iy)] = table
+    #             self.matrix[complex(ix + 1, iy)] = table
+    #             self.matrix[complex(ix + 2, iy)] = table
+    #             table += 1
+    #     self.ntables = table - 1
+    #     return self.matrix
+
+    def place_table(
+        self,
+        x: int,
+        y: int,
+        number: int,
+        vertical: bool = False,
+        length: int = 3,
+    ) -> bool:
+        if not (0 <= x < self.x and 0 <= y < self.y):
+            return False  # start position out of bounds
+
+        coords = []
+        if not vertical:
+            if x + length > self.x:
+                return False  # table does not fit
+            coords = [complex(ix, y) for ix in range(x, x + length)]
+        else:
+            if y + length > self.y:
+                return False  # table does not fit
+            coords = [complex(x, iy) for iy in range(y, y + length)]
+
+        if not all(self.matrix.get(xy, EMPTY) == EMPTY for xy in coords):
+            return False  # table overlaps with existing tables
+
+        # place table
+        for xy in coords:
+            self.matrix[xy] = number
+        return True
+
+    def room_matrix_str(self) -> str:
+        mat = self.room_matrix()
+        return "\n".join(" ".join(map(str, line)) for line in mat)
+
+    def room_matrix(self) -> list[list[int]]:
+        matrix = []
         for iy in range(self.y):
             line = []
             for ix in range(self.x):
-                line.append(self.matrix[complex(ix, iy)])
-            lines.append(" ".join(map(str, line)))
-        return "\n".join(lines)
+                line.append(self.matrix.get(complex(ix, iy), EMPTY))
+            matrix.append(line)
+        return matrix
